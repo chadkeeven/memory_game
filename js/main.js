b3d4bdbbd27fab6d8c3fb7294a236c1cddd606ae
Chad Keeven <chadkeeven@gmail.com>
2017-11-12 08:04:36 -0600

    added logic to flip cards

--- js/main.js
+++ js/main.js
@@ -31,21 +31,30 @@ var checkForMatch = function() {
 	}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	/* Code to send to console for debugging
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-
-
-		cardsInPlay.push(cards[cardId].rank);
+	*/
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}	
 };
 
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 
-
-flipCard(0);
-flipCard(2);
+createBoard();
 
 
